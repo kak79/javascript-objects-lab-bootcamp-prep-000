d0b85886edcb119ca562072378d7c9e7c6b610b3
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-15 11:16:08 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function updateObjectWithKeyAndValue(object, key, value){
+
+  return Object.assign({}, object, { [key]: value })
+   
+  
+}
+
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+  object[key] = value
+  return object
+  
+}
+
+
+function deleteFromObjectByKey(object, key){
+  
+  delete object.key
+  
+  
+  
+}
+
+
+
+
+
