8f83a533de114ce32ed28e2d908389394910ea65
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-15 12:16:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 function deleteFromObjectByKey(object, key){
   
   var newObject = Object.assign({}, object);
-  delete object.key;
+  delete newObject.key;
   return newObject;
   
 }
